111eed6cf5dad5cca7c81dc65cf3236e31020ae4
Carlos Mira-Perceval <ctmiraperceval@minsait.com>
2024-04-19 19:55:40 +0200

    c

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var intervalo;
 
 function iniciarCuentaRegresiva() {
-    detenerCuentaRegresiva();
-    var tiempoRestante = 15 * 60;
+    ocultarBotonInicio();
+    detenerCuentaRegresiva();  // Asegura que cualquier cuenta atrás anterior se detenga
+    var tiempoRestante = 15 * 60;  // 15 minutos
     actualizarTiempo(tiempoRestante);
     intervalo = setInterval(function() {
         tiempoRestante -= 1;
@@ -10,17 +11,15 @@ function iniciarCuentaRegresiva() {
         if (tiempoRestante <= 0) {
             clearInterval(intervalo);
             document.getElementById('cuentaRegresiva').textContent = "¡Tiempo agotado!";
+            // Aquí podrías también cambiar la visibilidad de otros botones si necesario
         }
     }, 1000);
 }
 
 function detenerCuentaRegresiva() {
-    if (intervalo) clearInterval(intervalo);
-}
-
-function reiniciarCuentaRegresiva() {
-    detenerCuentaRegresiva();
-    document.getElementById('cuentaRegresiva').textContent = "15:00";
+    if (intervalo) {
+        clearInterval(intervalo);
+    }
 }
 
 function actualizarTiempo(tiempoRestante) {
@@ -31,27 +30,9 @@ function actualizarTiempo(tiempoRestante) {
     document.getElementById('cuentaRegresiva').textContent = minutos + ":" + segundos;
 }
 
-document.getElementById('iniciarCuentaRegresiva').addEventListener('click', iniciarCuentaRegresiva);
-document.getElementById('detenerCuentaRegresiva').addEventListener('click', detenerCuentaRegresiva);
-document.getElementById('reiniciarCuentaRegresiva').addEventListener('click', reiniciarCuentaRegresiva);
-
-// Verifica si los Web Workers están disponibles
-if (window.Worker) {
-    var myWorker = new Worker('timerWorker.js');
-
-    myWorker.onmessage = function(e) {
-        var tiempoRestante = e.data;
-        var minutos = parseInt(tiempoRestante / 60, 10);
-        var segundos = parseInt(tiempoRestante % 60, 10);
-
-        minutos = minutos < 10 ? "0" + minutos : minutos;
-        segundos = segundos < 10 ? "0" + segundos : segundos;
-
-        document.getElementById('cuentaRegresiva').textContent = minutos + ":" + segundos;
-
-        if (tiempoRestante <= 0) {
-            document.getElementById('cuentaRegresiva').textContent = "¡Tiempo agotado!";
-            myWorker.terminate(); // Termina el worker si la cuenta atrás ha finalizado
-        }
-    };
+function ocultarBotonInicio() {
+    var botonInicio = document.getElementById('iniciarCuentaRegresiva');
+    botonInicio.style.display = 'none';  // Oculta el botón de iniciar
 }
+
+document.getElementById('iniciarCuentaRegresiva').addEventListener('click', iniciarCuentaRegresiva);
